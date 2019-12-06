b8ecfba4c9d0efbe3b398212c0522f01b09968fe
Tiago Rossi <tiagorferrer@hotmail.com>
2019-12-06 02:39:51 -0300

    versão final

--- RPN-Calculator.py
+++ RPN-Calculator.py
@@ -1,12 +1,41 @@
+def calcular(postfix):
+    auxStack = []
 
+    for i in postfix:
+        if i == "+":
+            n1 = auxStack.pop()
+            n2 = auxStack.pop()
+            aux = n1 + n2
+            auxStack.append(aux)
+        elif i == "-":
+            n1 = auxStack.pop()
+            n2 = auxStack.pop()
+            aux = n1 - n2
+            auxStack.append(aux)
+        elif i == "*":
+            n1 = auxStack.pop()
+            n2 = auxStack.pop()
+            aux = n1 * n2
+            auxStack.append(aux)
+        elif i == "/":
+            n1 = auxStack.pop()
+            n2 = auxStack.pop()
+            aux = n1 / n2
+            auxStack.append(aux)
+        elif i == "^":
+            n1 = auxStack.pop()
+            n2 = auxStack.pop()
+            aux = n1 ** n2
+            auxStack.append(aux)
+        else:
+            auxStack.append(i)
 
+    print('O resultado de ' + infix + ' é:')
+    print(auxStack)
 
 def convert(s):
     s = s.split(' ')
     s = list(s)
-    aux = ''
-    op = ''
-    numStack = []
     opStack = []
     postfix = []
 
@@ -21,38 +50,32 @@ def convert(s):
                     if opStack[(len(opStack)-2)] == '+' or (len(opStack)-2) == '-' or opStack[(len(opStack)-2)] == '*' or opStack[(len(opStack)-2)] == '/' or opStack[(len(opStack)-2)] == '^':
                         postfix.append(opStack[len(opStack) - 2])
                         del opStack[len(opStack) - 2]
-                        op = ''
                 elif len(opStack)>1 and (opStack[len(opStack)-1]) == '*' or (opStack[len(opStack)-1]) == '/':
                     if opStack[(len(opStack)-2)] == '*' or (len(opStack)-2) == '/' or (len(opStack)-2) == '^':
                         postfix.append(opStack[len(opStack) - 2])
                         del opStack[len(opStack) - 2]
-                        op = ''
                 elif len(opStack)>1 and (opStack[len(opStack)-1]) == '^':
                     if opStack[(len(opStack) - 2)] == '^':
                         postfix.append(opStack[len(opStack) - 2])
                         del opStack[len(opStack) - 2]
-                        op = ''
                 elif len(opStack)>1 and (opStack[len(opStack)-1]) == ')':
-                    for k in range (len(opStack)):
-                        if opStack[k] != ')':
-                            postfix.append(opStack[k])
-                            del opStack[k]
-                        else:
-                            del opStack[k]
-
-    if len(opStack) > 0:
-        for l in range (len(opStack)):
-            if opStack[l] != ')':
-                postfix.append(opStack[l])
-                del opStack[l]
+                    while len(opStack) > 1:
+                        postfix.append(opStack[len(opStack)-2])
+                        del opStack[len(opStack)-2]
+                    del opStack[len(opStack)-1]
 
 
-    print(opStack)
-    print()
+    if len(opStack) !=0:
+        for i in (opStack):
+            postfix.append(i)
+            opStack.pop()
+
     print('pilha RPN')
     print(postfix)
-    print(opStack)
-
+    calcular(postfix)
+print('----------------------------------------------------------------------------------------------------------')
+print('Para que o programa funcione corretamente, é necessário que tudo esteja espaçado, inclusive os parêntesis')
+print('----------------------------------------------------------------------------------------------------------')
 print('expressão infixa:')
 infix = input()
 
